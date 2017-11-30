d72ef92db454be84cfac28f808c821173fd03076
DruidKuma <druidkuma@gmail.com>
2017-11-30 03:22:06 +0200

    Project completed

---
+++ project_code.py
@@ -0,0 +1,332 @@
+import pickle
+import cv2
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from moviepy.editor import VideoFileClip
+
+def showImage(img):
+	plt.imshow(img, cmap='gray')
+	plt.show()
+
+
+def saveImage(img, name):
+    cv2.imwrite(name, img)
+
+
+def generateCameraCalibrationMatrix(img_width=1280, img_height=720):
+	#dimensions of chessboard on calibration images
+	grid_w = 9 #width
+	grid_h = 6 #height
+
+	images = glob.glob('camera_cal/calibration*.jpg')
+
+	objpoints = [] # 3D points in real world space
+	imgpoints = [] # 2D points in image plane
+
+	#object points for undistortion like (0,0,0), (1,0,0) up to (8,5,0)
+	objp = np.zeros((grid_h * grid_w, 3), np.float32)
+	objp[:,:2] = np.mgrid[0:grid_w, 0:grid_h].T.reshape(-1,2)
+
+	for fname in images:
+	    img = cv2.imread(fname)
+	    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	    # Find the chessboard corners
+	    ret, corners = cv2.findChessboardCorners(gray, (grid_w,grid_h), None)
+
+	    # If found, add object points, image points
+	    if ret == True:
+	        objpoints.append(objp)
+	        imgpoints.append(corners)
+
+	#calculate calibration matrix
+	ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, (img_width, img_height), None, None)
+
+	# Save the camera calibration result for later use
+	dist_pickle = {}
+	dist_pickle["mtx"] = mtx
+	dist_pickle["dist"] = dist
+	pickle.dump(dist_pickle, open( "calibration_result_pickle.p", "wb" ))
+
+
+def undistort(img, mtx, dist):
+    return cv2.undistort(img, mtx, dist, None, mtx)
+
+
+def unwarp(img, img_width=1280, img_height=720):
+    # Approximately for corner points of the region of interest
+    src = np.float32([[490, 482],[810, 482],
+                      [1250, 720],[40, 720]])
+
+    # Approximate target points
+    dst = np.float32([[0, 0], [1280, 0], 
+                     [1250, 720],[40, 720]])
+    
+    # Calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+
+    # Calculate the inverse perspective transform matrix
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    
+    # Warp the image
+    return cv2.warpPerspective(img, M, (img_width, img_height)), Minv
+
+
+def findLeftRightFit(binary_warped):
+	# Take a histogram of the bottom half of the image
+	histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):,:], axis=0)
+	# Create an output image to draw on and  visualize the result
+	out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+	# Find the peak of the left and right halves of the histogram
+	# These will be the starting point for the left and right lines
+	midpoint = np.int(histogram.shape[0]/2)
+	leftx_base = np.argmax(histogram[:midpoint])
+	rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+	# Choose the number of sliding windows
+	nwindows = 9
+	# Set height of windows
+	window_height = np.int(binary_warped.shape[0]/nwindows)
+	# Identify the x and y positions of all nonzero pixels in the image
+	nonzero = binary_warped.nonzero()
+	nonzeroy = np.array(nonzero[0])
+	
+	nonzerox = np.array(nonzero[1])
+	# Current positions to be updated for each window
+	leftx_current = leftx_base
+	rightx_current = rightx_base
+	# Set the width of the windows +/- margin
+	margin = 100
+	# Set minimum number of pixels found to recenter window
+	minpix = 50
+	# Create empty lists to receive left and right lane pixel indices
+	left_lane_inds = []
+	right_lane_inds = []
+
+	# Step through the windows one by one
+	for window in range(nwindows):
+	    # Identify window boundaries in x and y (and right and left)
+	    win_y_low = binary_warped.shape[0] - (window+1)*window_height
+	    win_y_high = binary_warped.shape[0] - window*window_height
+	    win_xleft_low = leftx_current - margin
+	    win_xleft_high = leftx_current + margin
+	    win_xright_low = rightx_current - margin
+	    win_xright_high = rightx_current + margin
+	    # Draw the windows on the visualization image
+	    cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
+	    (0,255,0), 2) 
+	    cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
+	    (0,255,0), 2) 
+	    # Identify the nonzero pixels in x and y within the window
+	    good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+	    (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+	    good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+	    (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+	    # Append these indices to the lists
+	    left_lane_inds.append(good_left_inds)
+	    right_lane_inds.append(good_right_inds)
+	    # If you found > minpix pixels, recenter next window on their mean position
+	    if len(good_left_inds) > minpix:
+	        leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+	    if len(good_right_inds) > minpix:        
+	        rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+	# Concatenate the arrays of indices
+	left_lane_inds = np.concatenate(left_lane_inds)
+	right_lane_inds = np.concatenate(right_lane_inds)
+
+	# Extract left and right line pixel positions
+	leftx = nonzerox[left_lane_inds]
+	lefty = nonzeroy[left_lane_inds] 
+	rightx = nonzerox[right_lane_inds]
+	righty = nonzeroy[right_lane_inds] 
+
+	# Fit a second order polynomial to each
+
+	left_fit = np.polyfit(lefty, leftx, 2)
+	right_fit = np.polyfit(righty, rightx, 2)
+
+	return left_fit, right_fit
+
+
+left_fit = []
+right_fit = []
+def detectLines(binary_warped):
+	global left_fit
+	global right_fit
+
+	if left_fit == [] or right_fit == []:
+		left_fit, right_fit = findLeftRightFit(binary_warped)
+
+	nonzero = binary_warped.nonzero()
+	nonzeroy = np.array(nonzero[0])
+	nonzerox = np.array(nonzero[1])
+	margin = 100
+	left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
+	left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
+	left_fit[1]*nonzeroy + left_fit[2] + margin))) 
+
+	right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + 
+	right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + 
+	right_fit[1]*nonzeroy + right_fit[2] + margin)))  
+
+	# Again, extract left and right line pixel positions
+	leftx = nonzerox[left_lane_inds]
+	lefty = nonzeroy[left_lane_inds] 
+	rightx = nonzerox[right_lane_inds]
+	righty = nonzeroy[right_lane_inds]
+	# Fit a second order polynomial to each
+	left_fit = np.polyfit(lefty, leftx, 2)
+	right_fit = np.polyfit(righty, rightx, 2)
+	# Generate x and y values for plotting
+	ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+	right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+	# Measure Radius of Curvature for each lane line
+	ym_per_pix = 30./720 # meters per pixel in y dimension
+	xm_per_pix = 3.7/700 # meteres per pixel in x dimension
+	left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
+	right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+	left_curverad = ((1 + (2*left_fit_cr[0]*np.max(lefty) + left_fit_cr[1])**2)**1.5) \
+	                             /np.absolute(2*left_fit_cr[0])
+	right_curverad = ((1 + (2*right_fit_cr[0]*np.max(lefty) + right_fit_cr[1])**2)**1.5) \
+	                                /np.absolute(2*right_fit_cr[0])
+	avg_curverad = int((left_curverad + right_curverad)/2)
+
+	return ploty, left_fitx, right_fitx, avg_curverad
+
+def drawLane(img, warped, Minv, ploty, left_fitx, right_fitx, avg_curverad):
+	# Create an image to draw the lines on
+	warp_zero = np.zeros_like(warped).astype(np.uint8)
+	color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+	# Recast the x and y points into usable format for cv2.fillPoly()
+	pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+	pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+	pts = np.hstack((pts_left, pts_right))
+
+	# Draw the lane onto the warped blank image
+	cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+	# Warp the blank back to original image space using inverse perspective matrix (Minv)
+	newwarp = cv2.warpPerspective(color_warp, Minv, (img.shape[1], img.shape[0])) 
+	# Combine the result with the original image
+	result = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
+
+	# Print radius of curvature on video
+	cv2.putText(result, 'Curve Radius {}(m)'.format(avg_curverad), (120,140),
+	         fontFace = 16, fontScale = 2, color=(255,255,255), thickness = 2)
+
+	return result
+
+
+def thresholdChannel(channel, thresh=(0,255)):
+	result = np.zeros(channel.shape, dtype=np.uint8)
+	result[(channel >= thresh[0]) & (channel <= thresh[1])] = 1
+	return result
+
+
+def colorThreshold(img):
+	hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+
+	s_binary = thresholdChannel(hls[:,:,2], thresh=(90,255))
+	h_binary = thresholdChannel(hls[:,:,0], thresh=(15,100))
+	r_binary = thresholdChannel(img[:,:,0], thresh=(200,255))
+
+	combined_binary = np.zeros_like(s_binary)
+
+	# H-chanel is used to exclude shadows, captured by S-channel.
+	# R-channel well captures white lines and forms a good union with S
+	combined_binary[((s_binary == 1) & (h_binary == 1)) | (r_binary == 1)] = 1
+
+	return combined_binary
+
+
+def gradientThreshold(img):
+	# Convert to gray
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	# Compute Sobel for bith X and Y
+	sobelX = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
+	sobelY = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=5)
+
+	absSobelX = np.absolute(sobelX)
+	absSobelY = np.absolute(sobelY)
+
+	# Form threshold binaries from Sobels
+	# Convert to 8 bit
+	scaledSobelX = np.uint8(255 * absSobelX / np.max(absSobelX))
+	scaledSobelY = np.uint8(255 * absSobelY / np.max(absSobelY))
+
+	x_binary = np.zeros(scaledSobelX.shape, dtype=np.uint8)
+	x_binary[(scaledSobelX >= 35) & (scaledSobelX <= 200)] = 1
+
+	y_binary = np.zeros(scaledSobelY.shape, dtype=np.uint8)
+	y_binary[(scaledSobelY >= 35) & (scaledSobelY <= 200)] = 1
+
+	# Form threshold Sobel magnitude binary
+	sobelMagnitude = np.sqrt(np.add(np.square(sobelX), np.square(sobelY)))
+	scaledSobelMagnitude = np.uint8(255 * sobelMagnitude / np.max(sobelMagnitude))
+	mag_binary = np.zeros(scaledSobelMagnitude.shape, dtype=np.uint8)
+	mag_binary[(scaledSobelMagnitude >= 50) & (scaledSobelMagnitude <= 250)] = 1
+
+	# Form threshold Direction Sobel binary
+	gradientDirection = np.arctan2(absSobelY, absSobelX)
+	dir_binary = np.zeros(gradientDirection.shape, dtype=np.uint8)
+	dir_binary[(gradientDirection > 0.7) & (gradientDirection < 1.3)] = 1
+
+	# Combine all gradients
+	combinedGradients = np.zeros_like(dir_binary)
+	combinedGradients[((x_binary == 1) & (y_binary == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+	return combinedGradients
+
+
+def thresholdPipeline(img):
+	color_binary = colorThreshold(img)
+	gradient_binary = gradientThreshold(img)
+
+	combined = np.zeros_like(color_binary)
+	combined[(gradient_binary == 1) | (color_binary == 1)] = 1
+
+	return combined
+
+
+def wholePipeline(img):
+	dist_pickle = pickle.load( open( "calibration_result_pickle.p", "rb" ) )
+	mtx = dist_pickle["mtx"]
+	dist = dist_pickle["dist"]
+
+	undistorted = undistort(img, mtx, dist)
+	thresholded = thresholdPipeline(undistorted)
+	unwarped, Minv = unwarp(thresholded)
+
+	ploty, left_fitx, right_fitx, avg_curverad = detectLines(unwarped)
+
+	result = drawLane(undistorted, unwarped, Minv, ploty, left_fitx, right_fitx, avg_curverad)
+	return result
+
+
+if __name__ == '__main__':
+	#generateCameraCalibrationMatrix()
+
+	image = cv2.imread('test_images/straight_lines1.jpg')
+	result = wholePipeline(image)
+	showImage(result)
+
+	# clip1 = VideoFileClip("project_video.mp4")
+	# white_clip = clip1.fl_image(wholePipeline)
+	# white_clip.write_videofile('project_result_video.mp4', audio=False)
+
+
+
+
+
+
+
+
+
+
---
+++ writeup.md
@@ -0,0 +1,181 @@
+#Advanced Lane Finding Project
+
+The goals / steps of this project are the following:
+
+* Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
+* Apply a distortion correction to raw images.
+* Use color transforms, gradients, etc., to create a thresholded binary image.
+* Apply a perspective transform to rectify binary image ("birds-eye view").
+* Detect lane pixels and fit to find the lane boundary.
+* Determine the curvature of the lane and vehicle position with respect to center.
+* Warp the detected lane boundaries back onto the original image.
+* Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
+
+[//]: # (Image References)
+
+[image1]: ./test_images/straight_lines1.jpg "Test Image"
+[image2]: ./output_images/camera_cal/corners_found5.jpg "Corners Example"
+[image3]: ./output_images/chess_dist.jpg "Chess Distorted"
+[image4]: ./output_images/chess_undist.jpg "Chess Undistorted"
+[image5]: ./output_images/test_undist.jpg "Test Undistorted"
+[image6]: ./output_images/s_channel_problem.png "S channel problem"
+[image7]: ./output_images/s_channel_binary.png "S channel binary"
+[image8]: ./output_images/h_channel_binary.png "H channel binary"
+[image9]: ./output_images/r_channel_binary.png "R channel binary"
+[image10]: ./output_images/color_thresh_combined.png "Combined Color Threshold"
+[image11]: ./output_images/sobel_x_binary.png "Sobel X Threshold"
+[image12]: ./output_images/sobel_y_binary.png "Sobel Y Threshold"
+[image13]: ./output_images/grad_mag_binary.png "Gradient Magnitude Threshold"
+[image14]: ./output_images/grad_dir_binary.png "Gradient Direction Threshold"
+[image15]: ./output_images/grad_combined_binary.png "Combined Gradient Threshold"
+[image16]: ./output_images/thresh_final.png "Final Threshold Result"
+[image17]: ./output_images/warped_result.png "Warped Result"
+[image18]: ./output_images/lines_detected.png "Line Detection Result"
+[image19]: ./output_images/final_result.png "Final Result"
+
+[video1]: ./project_result_video.mp4 "Video"
+
+## [Rubric](https://review.udacity.com/#!/rubrics/571/view) Points
+
+### Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
+
+---
+
+### Camera Calibration
+
+#### 1. Briefly state how you computed the camera matrix and distortion coefficients. Provide an example of a distortion corrected calibration image.
+
+The code for this step located in file `project_code.py` (lines 18-51).  
+
+I start by preparing "object points", which will be the (x, y, z) coordinates of the chessboard corners in the world. Here I am assuming the chessboard is fixed on the (9, 6) plane at z=0, such that the object points are the same for each calibration image.  Thus, `objp` is just a replicated array of coordinates, and `objpoints` will be appended with a copy of it every time I successfully detect all chessboard corners in a test image.  `imgpoints` will be appended with the (x, y) pixel position of each of the corners in the image plane with each successful chessboard detection.
+While detecting corners on the chessboard images, I draw detected corners to see everything goes right (all other images with corners can be found in output_images/camera_cal folder):
+
+![][image2]
+
+I then used the output `objpoints` and `imgpoints` to compute the camera calibration and distortion coefficients using the `cv2.calibrateCamera()` function.  I applied this distortion correction to the test image using the `cv2.undistort()` function and obtained this result: 
+
+![][image3]
+![][image4]
+
+I calculate distortion coefficients and matrix only once and save them into a file `calibration_result_pickle.p` for further use.
+
+### Pipeline (single images)
+
+#### 1. Provide an example of a distortion-corrected image.
+
+Using saved matrix and distortion coefficients, obtained from previous step, I applied `cv2.undistort()` to get the following result on the test image:
+
+![][image1]
+![][image5]
+
+
+#### 2. Describe how (and identify where in your code) you used color transforms, gradients or other methods to create a thresholded binary image.  Provide an example of a binary image result.
+
+I used a combination of color and gradient thresholds to generate a binary image.
+
+Firstly, I applied color thresholding to image. I chose converting an image to HSV to obtain H and S channels. S channel does pretty good job, but the problem with it is that it captures shadows, and it spoiled line detection:
+
+![][image6]
+
+H-channel appeared to be a good option to exlude shadows from S-channel. In addition to this pair I took R-channel from original RGB image which is good in capturing white lines.
+
+After playing a bit with threshold boundaries, I obtained the following results from color thresholding:
+
+**S-channel:**
+![][image7]
+
+**H-channel:**
+![][image8]
+
+**R-channel:**
+![][image9]
+
+**Combined:**
+![][image10]
+
+The code for color thresholding can be found in file `project_code.py` (lines 233-245)
+
+After this I also applied gradient thresholding
+I combined Sobel X with Sobel Y binary masks and gradient magnitude with gradient direction binary masks to obtain the following results:
+
+**Sobel X:**
+![][image11]
+
+**Sobel Y:**
+![][image12]
+
+**Graient Magnitude:**
+![][image13]
+
+**Graient Direction:**
+![][image14]
+
+**Combined:**
+![][image15]
+
+The code for gradient thresholding can be found in `project_code.py` file on lines 249-285.
+
+After these steps I combined color thresholding and gradient thresholding altogether and got the final result (lines 289-294):
+
+![][image16]
+
+#### 3. Describe how (and identify where in your code) you performed a perspective transform and provide an example of a transformed image.
+
+The code for my perspective transform includes a function called `unwarp()`, which appears in lines 59 through 80 in the file `project_code.py`. I chose to hardcode the source and destination points so that they fit the following mapping:
+
+| Source        | Destination   | 
+|:-------------:|:-------------:| 
+| 490, 482      | 0, 0        | 
+| 810, 482      | 1280, 0      |
+| 1250, 720     | 1250, 720      |
+| 40, 720      | 40, 720        |
+
+So, as could be seen from the point mapping, I left the bottom corners unwarped (mapped as is), and the two top points (defining the lanes pretty far from the car) are mapped to the top corners of the warped image.
+After applying such birds-eye effect on the above thresholded binary picture, I got the following result:
+
+![][image17]
+
+I also calculated the inverse matrix for unwarping the image with detected and colored lane.
+
+#### 4. Describe how (and identify where in your code) you identified lane-line pixels and fit their positions with a polynomial?
+
+Firstly I took a histogram along all the column in the lower half of the image to identify peaks (likely columns with lines) (lines 79-86). Then, starting from peaks positions, I use sliding windows, placed around the line centers to follow the lines up to the top of the frame and find pixels corresponding to left and right lines separately (lines 89-144). Last, but not least, I fitted the second order polynomials to each of lines (lines 148-149). As a result, I got the following:
+
+![][image18]
+
+I used global variables to apply sliding windows only once and slightly hurry up the processing of video stream (lines 154-155, 157-161)
+
+#### 5. Describe how (and identify where in your code) you calculated the radius of curvature of the lane and the position of the vehicle with respect to center.
+
+I did this on lines 188-197 on `project_code.py` file, measuring the average of the two curves to draw them on video.
+
+#### 6. Provide an example image of your result plotted back down onto the road such that the lane area is identified clearly.
+
+The final drawing of the results is located on lines 203-223 of the same `project_code.py` file.
+
+After doing all above steps I came up with the following result:
+
+![][image6]
+
+---
+
+### Pipeline (video)
+
+#### 1. Provide a link to your final video output.  Your pipeline should perform reasonably well on the entire project video (wobbly lines are ok but no catastrophic failures that would cause the car to drive off the road!).
+
+Here's a [link to my video result](./project_video.mp4)
+
+---
+
+### Discussion
+
+#### 1. Briefly discuss any problems / issues you faced in your implementation of this project.  Where will your pipeline likely fail?  What could you do to make it more robust?
+
+I faced a lot of issues with image preprocessing techniques. When I started the project, I suffered from thresholding either captures too much data (shadows) or don't capture lines at all (like in very light places). Only after endless trials I came up with a solution which satisfied me.
+ 
+Actually, the described pipeline works pretty well on the required project video, but fails on the challenges. I see a lot of places for further improving, e.g.: 
+
+* Not hardcode, but compute points of region of interest to be able to successfully detect lane on curvy road
+* Improve lane line points detection (as global variables are not a good code style)
+* Improve curvature measuring to satisfy the norms and compute average curvature along certain number of frames instead of for every frame separately
+* Implement proper tracking of line detection, best fit etc.
